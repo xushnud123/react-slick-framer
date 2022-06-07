2b135ae04612492ff4b772404df60f51f9aac569
Xushnudbek Raimov <12xushnudbek34@gmail.com>
2022-06-07 22:51:22 +0500

    finished

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 
+import {useEffect,useRef,useState} from 'react'
+import {motion} from 'framer-motion'
+import images from './images'
 import './App.css';
 
 function App() {
+  const [width,setWidth] = useState(0)
+  const carousel = useRef()
+
+  useEffect(()=>{
+    setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth)
+  },[])
   return (
     <div className="App">
-     
+     <motion.div ref={carousel} className='carousel' whileTap={{cursor:'grabbing'}}>
+      <motion.div drag="x" dragConstraints={{right:0,left:-width}} className='inner-carousel'>
+        {
+          images.map((img,index) => {
+            return (
+              <motion.div className='item' key={index}>
+                <img src={img} alt="" />
+              </motion.div>
+            )
+          })
+        }
+      </motion.div>
+     </motion.div>
     </div>
   );
 }
---
+++ src/images.js
@@ -0,0 +1,8 @@
+import images1 from './assets/hu-chen-5O6c_pLziXs-unsplash.jpg'
+import images2 from './assets/laurel-balyeat-ExarETx4xNA-unsplash.jpg'
+import images3 from './assets/hu-chen-5O6c_pLziXs-unsplash.jpg'
+import images4 from './assets/laurel-balyeat-ExarETx4xNA-unsplash.jpg'
+import images5 from './assets/hu-chen-5O6c_pLziXs-unsplash.jpg'
+import images6 from './assets/laurel-balyeat-ExarETx4xNA-unsplash.jpg'
+
+export default [images1,images2,images3,images4,images5,images6]
